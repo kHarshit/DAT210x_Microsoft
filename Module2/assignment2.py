d01528228b647fb55b9e60f0279349adf0438ac1
Harshit Kumar <kumar_harshit@outlook.com>
2017-04-09 22:37:55 +0530

    Solve Module2/assignment2

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -1,20 +1,23 @@
 import pandas as pd
+import os
+
+os.chdir("../Module1/")
+cwd = os.getcwd()
+print(cwd)
 
 # TODO: Load up the 'tutorial.csv' dataset
-#
-# .. your code here ..
-
-
+csv_df = pd.read_csv('tutorial.csv', sep=',')
 
 # TODO: Print the results of the .describe() method
-#
-# .. your code here ..
+print(csv_df)
+print(csv_df.describe())
+print(csv_df.info())
+print(csv_df.head(3))  # csv_df.iloc[:3, :] OR tail(); default no is 5
+print(csv_df.columns)  # display the name of the columns
+print(csv_df.index)
+print(csv_df.dtypes)  # what data type Pandas assigned each column
 
 
-
-# TODO: Figure out which indexing method you need to
-# use in order to index your dataframe with: [2:4,'col3']
-# And print the results
-#
-# .. your code here ..
+# TODO: Figure out which indexing method you need to use in order to index your dataframe with: [2:4,'col3']:
+print(csv_df.loc[2:4, 'col3'])  # .ix uses hybrid approach of selecting by column label and column index
 
